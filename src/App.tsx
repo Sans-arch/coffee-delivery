41a9a6f10b5b5c36b46516f13a2f160bece6e729
Santiago Fleitas <sansfleitas@gmail.com>
2023-03-20 11:32:45 -0300

    Create default theme, set fonts, set app colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-function App() {
+import { ThemeProvider } from "styled-components"
 
+import { GlobalStyle } from "./styles/global"
+import { defaultTheme } from "./styles/themes/default"
+
+function App() {
   return (
-    <h1>Hello World!</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
